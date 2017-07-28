7e2c62c9b5223cbfc4661d85f52bfa35fb82c29e
todddrinkwater <daniel-reason>
2017-07-28 15:37:52 +1200

    style changes, hope this works

--- client/components/Names.jsx
+++ client/components/Names.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
-// import names from '../../data/alphabet.js'
-// import {Link} from 'react-router-dom'
+import names from '../../data/alphabet.js'
+import {Link} from 'react-router-dom'
 
 
 export default function(props) {
-  // const letter = props.match.params.letter
-  // var searchResults = names.filter((lastname) => {
-  //   return lastname.split('')[0].toLowerCase() == this.letter
 
   return (
     <div className="letters">
       <h1>Hello World</h1>
-      // <ul>
-      //     {/* <li> {this.firstname + " " + this.lastname}</li> */}
-      //   })
-      // </ul>
     </div>
 
   )
-}
-//<Link to='/rank/:rank/:name'>{rankObject.name}</Link>
+ }
--- data/alphabet.js
+++ data/alphabet.js
@@ -2,43 +2,43 @@
 const alphabet = [
   {
     firstname: 'Aaron',
-    lastname: 'Macdonald'
+    lastname: 'Macdonald',
     address: '',
     phone: 022 0922 222
   },
   {
     firstname: 'Ania',
-    lastname: 'Podhajska'
+    lastname: 'Podhajska',
     address: '',
     phone: 022 0922 222
   },
   {
     firstname: 'Annelise',
-    lastname: 'Hartono'
+    lastname: 'Hartono',
     address: '',
     phone: 022 0922 222
   },
   {
     firstname: 'Anthony',
-    lastname: 'Martin'
+    lastname: 'Martin',
     address: '',
     phone: 022 0922 222
   },
   {
     firstname: 'Annah',
-    lastname: 'Gerletti'
+    lastname: 'Gerletti',
     address: '',
     phone: 022 0922 222
   },
   {
     firstname: 'Brian',
-    lastname: 'Bemis'
+    lastname: 'Bemis',
     address: '',
     phone: 022 0922 222
   },
   {
     firstname: 'Chris',
-    lastname: 'Chou'
+    lastname: 'Chou',
     address: '',
     phone: 022 0922 222
   }
