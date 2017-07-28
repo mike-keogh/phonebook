c471f5798e3497fc768f8d645a978d7b526befa3
todddrinkwater <daniel-reason>
2017-07-28 14:02:22 +1200

    nav bar shows

---
+++ client/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+// const alphabet = [
+//   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+// ]
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+const Nav = () => {
+  return (
+    <div className='navBar'>
+      <ul>
+        {alphabet.toUpperCase().split('').map((letter, i) => <li>{letter}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
